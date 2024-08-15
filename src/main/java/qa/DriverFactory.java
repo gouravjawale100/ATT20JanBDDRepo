418cfda098b349628e8b2a06808ceb10429e4fd7
Gourav <gourav@g.com>
2024-08-15 21:32:33 +0530

    modified driverfactory

--- src/main/java/qa/DriverFactory.java
+++ src/main/java/qa/DriverFactory.java
@@ -1,8 +1,13 @@
 package qa;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
 	static WebDriver driver;
